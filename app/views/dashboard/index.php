1f80e5207f559f1e539563d5147ea2536b300211
adelia <152515061+adeliasakinah@users.noreply.github.com>
2023-11-30 22:29:25 +0700

    Initial commit

---
+++ app/views/dashboard/index.php
@@ -0,0 +1,36 @@
+<style>
+    .info {
+        font-size: 18px;
+        margin-bottom: 20px;
+    }
+</style>
+</head>
+
+<body>
+    <h2>Dashboard</h2>
+
+    <div class="info" id="welcome-message">Selamat Datang Di Program ini</div><br>
+
+    <script>
+        function updateClock() {
+            var now = new Date();
+            var hours = now.getHours();
+            var minutes = now.getMinutes();
+            var seconds = now.getSeconds();
+
+            // Tambahkan nol di depan angka jika kurang dari 10
+            hours = hours < 10 ? "0" + hours : hours;
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            var timeString = hours + ":" + minutes + ":" + seconds;
+
+            document.getElementById("welcome-message").innerHTML = "Selamat datang <br><br> Jam : <br> " + timeString;
+        }
+
+        // Panggil fungsi updateClock setiap detik
+        setInterval(updateClock, 1000);
+
+        // Panggil fungsi untuk pertama kali saat halaman dimuat
+        updateClock();
+    </script>
